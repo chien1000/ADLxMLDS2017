e23f48aa23a260f22fa3a14ab9632ac52abae45e
ChienHui <r05725004@ntu.edu.tw>
2017-12-11 19:32:21 +0800

    eval and target network, update by frequency

--- hw3/agent_dir/agent_dqn.py
+++ hw3/agent_dir/agent_dqn.py
@@ -9,6 +9,7 @@ import numpy as np
 import math
 from itertools import count
 import pickle
+import json
 import time
 import os
 
@@ -18,15 +19,27 @@ import matplotlib.pyplot as plt
 
 BATCH_SIZE = 32 #128
 GAMMA = 0.999
+TARGET_UPDATE_FREQ = 100
+EVAL_UPDATE_FREQ = 4
+
+NUM_EPISODES = 40000
+ENV_STEPS = 10000000
+
+DECAY_STEPS  = ENV_STEPS / 10
 EPS_START = 0.9
 EPS_END = 0.05
-EPS_DECAY = 200
-NUM_EPISODES = 40000
+EPS_DECAY =  DECAY_STEPS / 10 #200
+
 PRINT_EVERY = 30
 SAVE_EVERY = 200
 LEARNING_RATE = 0.01
 SAVE_PREFIX = ''
 SAVE_PATH = 'models'
+
+PARAMS = {'BATCH_SIZE':BATCH_SIZE, 'GAMMA':GAMMA, 
+        'TARGET_UPDATE_FREQ':TARGET_UPDATE_FREQ, 'EVAL_UPDATE_FREQ':EVAL_UPDATE_FREQ,  
+          'NUM_EPISODES':NUM_EPISODES,  'ENV_STEPS':ENV_STEPS, 'DECAY_STEPS':DECAY_STEPS,
+          'EPS_START': EPS_START, 'EPS_END':EPS_END, 'EPS_DECAY':EPS_DECAY, 'LEARNING_RATE':LEARNING_RATE,}
 
 # if gpu is to be used
 use_cuda = torch.cuda.is_available()
@@ -46,21 +59,23 @@ class Agent_DQN(Agent):
         super(Agent_DQN,self).__init__(env)
         self.action_count = env.get_action_space().n
 
-        self.model = DQN(self.action_count)
+        self.eval_model = DQN(self.action_count)
+        self.target_model = DQN(self.action_count)
         if use_cuda:
-            self.model.cuda()
+            self.eval_model.cuda()
+            self.target_model.cuda()
         
 
         if args.test_dqn:
             #you can load your model here
             self.model_path = args.model_path
-            self.model.load_state_dict(torch.load(self.model_path, map_location={'cuda:0': 'cpu','cuda:1':'cpu','cuda:2':'cpu','cuda:3':'cpu'}))
+            self.eval_model.load_state_dict(torch.load(self.model_path, map_location={'cuda:0': 'cpu','cuda:1':'cpu','cuda:2':'cpu','cuda:3':'cpu'}))
             if use_cuda:
-                self.model.cuda()
+                self.eval_model.cuda()
             print('loading trained model')
 
         if args.train_dqn:
-            self.optimizer = optim.RMSprop(self.model.parameters(), lr=LEARNING_RATE)
+            self.optimizer = optim.RMSprop(self.eval_model.parameters(), lr=LEARNING_RATE)
             self.memory = ReplayMemory(10000)
 
             self.steps_done = 0
@@ -74,7 +89,7 @@ class Agent_DQN(Agent):
                 ts = "%d"%(time.time())
                 self.save_path = self.save_path+ '_' + ts
                 os.makedirs(self.save_path)
-            # json.dump(PARAMS, open(os.path.join(self.save_path, 'params.json'),'w'))        
+            json.dump(PARAMS, open(os.path.join(self.save_path, 'params.json'),'w'))        
         ##################
         # YOUR CODE HERE #
         ##################
@@ -102,7 +117,7 @@ class Agent_DQN(Agent):
 
     def train(self):
         # import pdb; pdb.set_trace()
-        for i_episode in range(1, NUM_EPISODES+1):
+        for i_episode in count(1): #range(1, NUM_EPISODES+1):
             print('Episode {}/{}'.format(i_episode, NUM_EPISODES))
             # Initialize the environment and state
             q_reward = 0.0
@@ -124,13 +139,15 @@ class Agent_DQN(Agent):
 
                 # Move to the next state
                 observation = observation_next
+                self.steps_done += 1
 
                 # Perform one step of the optimization (on the target network)
-                self.optimize_model()
+                if self.steps_done % EVAL_UPDATE_FREQP == 0:
+                    self.optimize_model()
                 if done:
                     self.episode_rewards.append(q_reward)
                     if i_episode % PRINT_EVERY == 0:
-                        #self.plot_rewards()
+                        # self.plot_rewards()
                         print(self.episode_rewards[-29:])
                     break
 
@@ -140,11 +157,14 @@ class Agent_DQN(Agent):
                     # pickle.dump((self.episode_rewards), open(os.path.join(self.save_path,'rewards.pkl'), 'wb'))
                     with open(os.path.join(self.save_path,'rewards.txt'), 'w') as f:
                         f.write('\n'.join(list(map(str,self.episode_rewards))))
-                    torch.save(self.model.state_dict(), open(os.path.join(self.save_path, 'epoch_{}_model.bin'.format(i_episode)), 'wb'))
+                    torch.save(self.eval_model.state_dict(), open(os.path.join(self.save_path, 'epoch_{}_model.bin'.format(i_episode)), 'wb'))
                     print('--- save model ---')
                 except Exception as e:
                     print('error occurs when saving data')
                     print(str(e))
+
+            if self.steps_done > ENV_STEPS:
+                break
 
         print('Complete')
         self.env.env.render(close=True)
@@ -169,16 +189,17 @@ class Agent_DQN(Agent):
         # YOUR CODE HERE #
         ##################
         if test:
-            #TODO: max outputs?????
             observation = self.prepro_observation(observation)
-            return self.model(Variable(observation, volatile=True).type(FloatTensor)).data.max(1)[1][0] #[1]: index matrix
+            # return self.model(Variable(observation, volatile=True).type(FloatTensor)).data.max(1)[1][0] #[1]: index matrix
+            return self.eval_model(Variable(observation).type(FloatTensor)).data.max(1)[1][0] #[1]: index matrix
         else:
             sample = random.random()
             eps_threshold = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * self.steps_done / EPS_DECAY)
-            self.steps_done += 1
+            # self.steps_done += 1
             # print('steps {}, eps_threshold {}'.format(self.steps_done, eps_threshold))
             if sample > eps_threshold:
-                return self.model(Variable(observation, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
+                # return self.model(Variable(observation, volatile=True).type(FloatTensor)).data.max(1)[1].view(1, 1)
+                return self.eval_model(Variable(observation).type(FloatTensor)).data.max(1)[1].view(1, 1)
             else:
                 return LongTensor([[random.randrange(self.action_count)]])
 
@@ -186,6 +207,9 @@ class Agent_DQN(Agent):
     def optimize_model(self):
         if len(self.memory) < self.memory.capacity:
             return
+
+        if self.steps_done % TARGET_UPDATE_FREQ == 0:
+            self.target_model.load_state_dict(self.eval_model.state_dict())
         # import pdb; pdb.set_trace()
         transitions = self.memory.sample(BATCH_SIZE)
         # Transpose the batch
@@ -197,8 +221,8 @@ class Agent_DQN(Agent):
         # We don't want to backprop through the expected action values and volatile
         # will save us on temporarily changing the model parameters'
         # requires_grad to False!
-        non_final_next_states = Variable(torch.cat([s for s in batch.next_state
-                                                    if s is not None]), volatile=True)
+        # non_final_next_states = Variable(torch.cat([s for s in batch.next_state if s is not None]), volatile=True)
+        non_final_next_states = Variable(torch.cat([s for s in batch.next_state if s is not None]))
                                         
         state_batch = Variable(torch.cat(batch.state))
         action_batch = Variable(torch.cat(batch.action))
@@ -206,15 +230,16 @@ class Agent_DQN(Agent):
 
         # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
         # columns of actions taken
-        state_action_values = self.model(state_batch).gather(1, action_batch)
+        state_action_values = self.eval_model(state_batch).gather(1, action_batch)
 
         # Compute V(s_{t+1}) for all next states.
         next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
-        next_state_values[non_final_mask] =self.model(non_final_next_states).max(1)[0]
+        next_state_predict_values = self.target_model(non_final_next_states).detach()
+        next_state_values[non_final_mask] = next_state_predict_values.max(1)[0]
         # Now, we don't want to mess up the loss with a volatile flag, so let's
         # clear it. After this, we'll just end up with a Variable that has
         # requires_grad=False
-        next_state_values.volatile = False
+        # #next_state_values.volatile = False
         # Compute the expected Q values
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch
 
